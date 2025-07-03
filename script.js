80a0f5a3abf159bd67362857fa44641c3bd867fa
jacintoql <jequintana@queretaro.gob.mx>
2025-07-03 09:06:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const universities = {
     name: "Universidad Politécnica de Querétaro",
     img: "LogosUniversidades/LOGO_UPQ.png",
     courses: [
-      "LICENCIATURA EN COMERCIO INTERNACIONAL Y ADUANAS",
-      "LICENCIATURA EN ADMINISTRACIÓN",
-      "LICENCIATURA EN INGENIERÍA MECATRÓNICA",
-      "LICENCIATURA EN INGENIERÍA MECATRÓNICA (MODALIDAD MIXTA)",
-      "LICENCIATURA EN INGENIERÍA EN MANUFACTURA AVANZADA",
-      "LICENCIATURA EN INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN E INNOVACIÓN DIGITAL",
-      "LICENCIATURA EN INGENIERÍA EN TECNOLOGÍA AUTOMOTRIZ",
-      "LICENCIATURA EN INGENIERÍA DE DATOS E INTELIGENCIA ARTIFICIAL"
+      "Licenciatura en Comercio Internacional y Aduanas",
+      "Licenciatura en Administración",
+      "Ingeniería Mecatrónica",
+      "Ingeniería Mecatrónica (Modalidad Mixta)",
+      "Ingeniería en Manufactura Avanzada",
+      "Ingeniería en Tecnologías de la Información e Innovación Digital",
+      "Ingeniería en Tecnología Automotriz",
+      "Ingeniería de Datos e Inteligencia Artificial"
     ],
     convocationLink: "https://sistemaintegral.upq.edu.mx/admisiones.php"
   },
@@ -19,12 +19,12 @@ const universities = {
     name: "Universidad Politécnica de Santa Rosa",
     img: "LogosUniversidades/LOGO_UP.png",
     courses: [
-      "INGENIERÍA EN CALIDAD Y METROLOGÍA",
-      "INGENIERÍA EN SISTEMAS AUTOMOTRICES",
-      "INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN E INNOVACIÓN DIGITAL",
-      "INGENIERÍA EN ANIMACIÓN Y EFECTOS VISUALES",
-      "LICENCIATURA EN TERAPIA FÍSICA",
-      "INGENIERÍA EN ROBÓTICA COMPUTACIONAL"
+      "Ingeniería en Calidad y Metrología",
+      "Ingeniería en Sistemas Automotrices",
+      "Ingeniería en Tecnologías de la Información e Innovación Digital",
+      "Ingeniería en Animación y Efectos Visuales",
+      "Licenciatura en Terapia Física",
+      "Ingeniería en Robótica Computacional"
     ],
     convocationLink: "https://upsrj.edu.mx/inscripciones-reinscripciones/"
   },
@@ -32,21 +32,21 @@ const universities = {
     name: "Universidad Tecnológica de Querétaro",
     img: "LogosUniversidades/LOGO_HALCON.png",
     courses: [
-      "LICENCIATURA EN ADMINISTRACIÓN",
-      "LICENCIATURA EN NEGOCIOS Y MERCADOTECNÍA",
-      "LICENCIATURA EN INGENIERÍA EN LOGÍSTICA",
-      "LICENCIATURA EN EDUCACIÓN",
-      "LICENCIATURA EN INGENIERÍA AMBIENTAL Y SUSTENTABILIDAD",
-      "LICENCIATURA EN INGENIERÍA EN ENERGÍA Y DESARROLLO SOSTENIBLE",
-      "LICENCIATURA EN INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN E INNOVACIÓN DIGITAL",
-      "LICENCIATURA EN INGENIERÍA MECATRÓNICA",
-      "LICENCIATURA EN INGENIERÍA EN MANTENIMIENTO INDUSTRIAL",
-      "LICENCIATURA EN INGENIERÍA EN NANOTECNOLOGÍA",
-      "LICENCIATURA EN INGENIERÍA INDUSTRIAL",
-      "LICENCIATURA EN INGENIERÍA MECÁNICA",
-      "LICENCIATURA EN INGENIERÍA MECÁNICA AUTOMOTRIZ",
-      "LICENCIATURA EN INGENIERÍA SEMICONDUCTORES",
-      "LICENCIATURA EN CONTADURÍA"
+      "Licenciatura En Administración",
+      "Licenciatura en Negocios y Mercadotecnía",
+      "Licenciatura en Ingeniería en Logística",
+      "Licenciatura en Educación",
+      "Licenciatura en Ingeniería Ambiental y Sustentabilidad",
+      "Licenciatura en Ingeniería En Energía y Desarrollo Sostenible",
+      "Licenciatura en Ingeniería en Tecnologías de la Información e Innovación Digital",
+      "Licenciatura en Ingeniería Mecatrónica",
+      "Licenciatura en Ingeniería en Mantenimiento Industrial",
+      "Licenciatura en Ingeniería en Nanotecnología",
+      "Licenciatura en Ingeniería Industrial",
+      "Licenciatura en Ingeniería Mecánica",
+      "Licenciatura en Ingeniería Mecánica Automotriz",
+      "Licenciatura en Ingeniería Semiconductores",
+      "Licenciatura en Contaduría"
     ],
     convocationLink: "https://admisiones.uteq.edu.mx/"
   },
@@ -54,11 +54,11 @@ const universities = {
     name: "Universidad Tecnológica de Corregidora",
     img: "LogosUniversidades/LOGO_UTC.png",
     courses: [
-      "LICENCIATURA EN GESTIÓN Y DESARROLLO TURÍSTICO",
-      "LICENCIATURA EN INGENIERÍA EN MANTENIMIENTO INDUSTRIAL",
-      "LICENCIATURA EN INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN E INNOVACIÓN DIGITAL",
-      "LICENCIATURA EN INGENIERÍA EN BIOTECNOLOGÍA",
-      "LICENCIATURA EN INGENIERÍA MECATRÓNICA"
+      "Licenciatura en Gestión y Desarrollo Turístico",
+      "Licenciatura en Ingeniería en Mantenimiento Industrial",
+      "Licenciatura en Ingeniería en Tecnologías de la Información e Innovación Digital",
+      "Licenciatura en Ingeniería en Biotecnología",
+      "Licenciatura en Ingeniería Mecatrónica"
     ],
     convocationLink: "https://captacion.utcorregidora.edu.mx/"
   },
@@ -66,10 +66,10 @@ const universities = {
     name: "Universidad Aeronáutica de Querétaro",
     img: "LogosUniversidades/LOGO_UNAQ.png",
     courses: [
-      "INGENIERÍA AERONÁUTICA EN MANUFACTURA",
-      "INGENIERÍA EN DISEÑO MECÁNICO AERONÁUTICO",
-      "INGENIERÍA EN ELECTRÓNICA Y CONTROL DE SISTEMAS DE AERONAVES",
-      "INGENIERÍA AERONÁUTICA EN MANTENIMIENTO"
+      "Ingeniería Aeronáutica en Manufactura",
+      "Ingeniería en Diseño Mecánico Aeronáutico",
+      "Ingeniería en Electrónica y Control de Sistemas de Aeronaves",
+      "Ingeniería Aeronáutica en Mantenimiento"
     ],
     convocationLink: "https://www.unaq.edu.mx/admisiones/convocatoria-ingenieria/"
   },
@@ -77,24 +77,22 @@ const universities = {
     name: "Universidad Tecnológica de San Juan del Río",
     img: "LogosUniversidades/LOGO_UTSJR.png",
     courses: [
-      "TSU EN MERCADOTECNIA (JALPAN). LICENCIATURA EN NEGOCIOS Y MERCADOTECNIA",
-      "TSU EN MERCADOTECNIA (SJR). LICENCIATURA EN NEGOCIOS Y MERCADOTECNIA",
-      "TSU EN QUÍMICA TECNOLÓGICA FARMACÉUTICA. LIC. E INGENIERÍA QUÍMICA FARMACÉUTICA",
-      "TSU EN QUÍMICA INDUSTRIAL. LICENCIATURA EN INGENIERÍA QUÍMICA",
-      "TSU EN ENERGÍA TURBO SOLAR. LIC. EN ING. ENERGÍA Y DESARROLLO SUSTENTABLE",
-      "TSU EN CONSTRUCCIÓN. LICENCIATURA EN INGENIERÍA CIVIL",
-      "TSU EN AUTOMOTRIZ. LICENCIATURA EN INGENIERÍA INDUSTRIAL",
-      "TSU EN PROCESOS PRODUCTIVOS. LICENCIATURA EN INGENIERÍA INDUSTRIAL",
-      "TSU EN SISTEMAS DE GESTIÓN DE CALIDAD. LICENCIATURA EN INGENIERÍA INDUSTRIAL",
-      "TSU EN DESARROLLO DE SOFTWARE MULTIPLATAFORMA (JALPAN). INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN E INNOVACIÓN DIGITAL",
-      "TSU EN DESARROLLO DE SOFTWARE MULTIPLATAFORMA (SJR). INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN E INNOVACIÓN DIGITAL",
-      "TSU EN ENTORNOS VIRTUALES Y NEGOCIOS DIGITALES. INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN E INNOVACIÓN DIGITAL",
-      "TSU EN INTELIGENCIA ARTIFICIAL - INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN E INNOVACIÓN DIGITAL",
-      "TSU EN AUTOMATIZACIÓN. LICENCIATURA EN INGENIERÍA MECATRÓNICA",
-      "TSU EN ROBÓTICA. LICENCIATURA EN INGENIERÍA MECATRÓNICA",
-      "TSU EN AUTOMATIZACIÓN. LIC. INGENIERÍA MECATRÓNICA MIXTO (VIRTUAL)",
-      "TSU EN MANTENIMIENTO INDUSTRIAL. LICENCIATURA EN INGENIERÍA EN MANTENIMIENTO INDUSTRIAL",
-      "TSU EN TURISMO. LIC. GESTIÓN Y DESARROLLO TURÍSTICO"
+      "TSU en Mercadotecnia (Jalpan y SJR). Lic. en Negocios Y Mercadotecnia",
+      "TSU en Química Tecnológica Farmacéutica. Ing. Química Farmacéutica",
+      "TSU en Química Industrial. Ingeniería Química",
+      "TSU en Energía Turbo Solar. Ing. Energía Y Desarrollo Sustentable",
+      "TSU en Construcción. Ingeniería Civil",
+      "TSU en Automotriz. Ingeniería Industrial",
+      "TSU en Procesos Productivos. Ingeniería Industrial",
+      "TSU en Sistemas de Gestión de Calidad. Ingeniería Industrial",
+      "TSU en Desarrollo de Software Multiplataforma (Jalpan y SJR). Ing. en Tec. de la Información e Innovación Digital",
+      "TSU en Entornos Virtuales Y Negocios Digitales. Ing. en Tec.de la Información e Innovación Digital",
+      "TSU en IA – Ing. en Tec. de la Información e Innovación Digital",
+      "TSU en Automatización. Ingeniería Mecatrónica",
+      "TSU en Robótica. Ingeniería Mecatrónica",
+      "TSU en Automatización. Ingeniería Mecatrónica Mixto (Virtual)",
+      "TSU en Mantenimiento Industrial. Ing. en Mantenimiento Industrial",
+      "TSU en Turismo. Lic. Gestión Y Desarrollo Turístico"
     ],
     convocationLink: "http://200.188.14.50/admision/admision.jsp?C=2"
   },
@@ -102,18 +100,18 @@ const universities = {
     name: "TECNM Campus Querétaro",
     img: "LogosUniversidades/LOGO_ITQ.png",
     courses: [
-      "INGENIERÍA EN SISTEMAS COMPUTACIONALES",
-      "INGENIERÍA ELECTRÓNICA",
-      "INGENIERÍA INDUSTRIAL",
-      "INGENIERÍA MECÁNICA",
-      "INGENIERÍA MECATRÓNICA",
-      "INGENIERÍA ELÉCTRICA",
-      "INGENIERÍA EN MATERIALES",
-      "INGENIERÍA EN SEMICONDUCTORES",
-      "INGENIERÍA EN CIENCIA DE DATOS",
-      "INGENIERÍA EN LOGÍSTICA",
-      "LICENCIATURA EN ARQUITECTURA",
-      "LICENCIATURA EN GESTIÓN EMPRESARIAL"
+      "Ingeniería en Sistemas Computacionales",
+      "Ingeniería Electrónica",
+      "Ingeniería Industrial",
+      "Ingeniería Mecánica",
+      "Ingeniería Mecatrónica",
+      "Ingeniería Eléctrica",
+      "Ingeniería en Materiales",
+      "Ingeniería en Semiconductores",
+      "Ingeniería en Ciencia De Datos",
+      "Ingeniería en Logística",
+      "Licenciatura en Arquitectura",
+      "Licenciatura en Gestión Empresarial"
     ],
     convocationLink: "https://queretaro.tecnm.mx/wp-content/uploads/2024/06/CONVOCATORIA-NUEVO-INGRESO-AGO-DIC-2024-Extraordinaria-1.pdf"
   },
@@ -121,12 +119,12 @@ const universities = {
     name: "TECNM Campus San Juan del Río",
     img: "LogosUniversidades/LOGO_ITQ.png",
     courses: [
-      "INGENIERÍA INDUSTRIAL",
-      "INGENIERÍA EN SISTEMAS COMPUTACIONALES",
-      "INGENIERÍA ELECTRÓNICA",
-      "INGENIERÍA EN GESTIÓN EMPRESARIAL",
-      "INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN Y COMUNICACIONES",
-      "INGENIERÍA EN ELECTROMECÁNICA"
+      "Ingeniería Industrial",
+      "Ingeniería en Sistemas Computacionales",
+      "Ingeniería Electrónica",
+      "Ingeniería en Gestión Empresarial",
+      "Ingeniería en Tecnologías de la Información y Comunicaciones",
+      "Ingeniería en Electromecánica"
     ],
     convocationLink: "https://cetech.sjuanrio.tecnm.mx/convocatoriaAdmision/6462448e1fcfda3330afe6fc5b50d53b"
   }
